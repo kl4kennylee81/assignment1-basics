1b875c4b4af22d881b330feafa89c25c6ec35a29
Kenneth Lee <kennethlee@kenneths-mbp.mynetworksettings.com>
2025-07-18 23:10:12 -0400

    generate text

--- cs336_basics/transformer.py
+++ cs336_basics/transformer.py
@@ -8,7 +8,6 @@ from einops import einsum, rearrange
 
 
 class Linear(nn.Module):
-
     def __init__(self, in_features:int, out_features:int, device: torch.device | None = None, dtype: torch.dtype | None =None):
         super().__init__()
         W = torch.empty(out_features, in_features, device=device, dtype=dtype)
